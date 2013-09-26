1a448e4d395cc2908a0ef37756ed6a39011d420e
SimonHFrost <simonhfrost@gmail.com>
2013-09-26 22:30:23 +1200

    detect if first argument is a haiku

--- haiku_holla.rb
+++ haiku_holla.rb
@@ -1,11 +1,30 @@
 require 'lingua'
+require 'set'
 
 if (ARGV.length != 1)
   raise "Usage: haiku_holla.rb [text]"
 end
 
-@text = Lingua::EN::Readability.new(ARGV[0])
+@report = Lingua::EN::Readability.new(ARGV[0])
+@syllables = @report.num_syllables
 
 print 'There are '
-print @text.num_syllables
+print @syllables
 puts ' syllables in the supplied input.'
+
+if (@syllables != 17)
+  abort("This isn't the correct number of syllables for a haiku.")
+end
+
+@words = ARGV[0].split(" ")
+@syllabelCountMatch = Set.new [5, 12, 17]
+@totalSyllabels = 0
+@syllabelSet = Set.new
+@words.each do |item|
+  @report = Lingua::EN::Readability.new(item)
+  @totalSyllabels += @report.num_syllables
+  @syllabelSet.add(@totalSyllabels)
+end
+
+print 'Haiku: '
+puts @syllabelCountMatch.subset? @syllabelSet
